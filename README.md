8031dc9b0e5fbfc09a13322bbaae96e9558d477f
Danissss <danis.cao@hotmail.com>
2019-06-20 10:50:07 -0600

    new blast db

--- README.md
+++ README.md
@@ -11,3 +11,46 @@ uploaders: keep track of uploaders' information
 
 # Table Explaination
 nucleotide_sequences: update_date and publication_date are different, make publication_date as primary reference.
+
+
+# for blast+ installation
+## ubuntu
+apt update
+apt upgrade
+apt install ncbi-blast+-legacy
+apt install ncbi-blast+
+
+note: ubuntu doesn't need to worry about the issue from MacOS since the blastall program can be installed 
+through apt install ncbi-blast+-legacy
+
+## MacOS
+Below instruction is for using old blastall program 
+BioRuby still use blastall so currently have to use legacy_blast.pl
+
+Use legacy_blast.pl in /usr/local/ncbi/blast/bin for ruby on rails
+then override following function 
+see reference: https://stackoverflow.com/questions/580314/overriding-a-module-method-from-a-gem-in-rails
+
+module Bio
+  class Blast
+    def make_command_line
+      path = "/path/to/ncbi-blast-2.9.0+/bin"  # CHANGE ME
+      cmd = make_command_line_options
+      cmd.unshift @blastall
+      cmd.unshift "#{path}/legacy_blast.pl"    # ADDED
+      cmd.push "--path"                        # ADDED
+      cmd.push path                            # ADDED
+      cmd
+    end
+  end
+end
+
+program = "blastn"
+blast_options = "-m 8"
+
+blaster = Bio::Blast.local(program, "#{Rails.root}/index/blast/#{database}", blast_options)
+report = blaster.query(@sequence.seq)
+
+IMPORTANT:
+legacy_blast.pl use default /usr/bin for all executable blast program.
+if use legacy_blast.pl, should modify the legacy_blast.pl
--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -32,20 +32,65 @@ class QueryController < ApplicationController
         @sequence.auto # Guess the type of sequence. Changes the class of @sequence.
         query_sequence_type = @sequence.seq.class == Bio::Sequence::AA ? 'protein' : 'gene'
         program = @sequence.seq.class == Bio::Sequence::AA ? 'blastp' : 'blastn'
-        database = query_sequence_type == 'protein' ? 'protein' : 'gene'
+        database = query_sequence_type == 'protein' ? 'reductive_dehalogenase_protein' : 'reductive_dehalogenase_gene'
+        # sequence_class is for search through the sequence database
+        # sequence_class = query_sequence_type == 'protein' ? ProteinSequence : NucleotideSequence
+
 
         blast_options = { 'e' => params[:evalue].to_f, 'G' => params[:gap_cost].to_i,
                           'E' => params[:extend_cost].to_i, 'q' => params[:mismatch_penalty].to_i, 'r' => params[:match_reward].to_i,
                           'U' => (params[:lower_case_filtering].present? ? 'T' : 'F'), 
                           'F' => (params[:filter_query_sequence].present? ? 'T' : 'F'),
                           'g' => (params[:gapped_alignment].present? ? 'T' : 'F') }
-        blast_options = blast_options.collect { |key, value| value.present? ? "-#{key} #{value}" : nil }.compact.join(" ")
 
+        blast_options = blast_options.collect { |key, value| value.present? ? "-#{key} #{value}" : nil }.compact.join(" ")
+            # # *Arguments*:
+            # # * _program_ (required): 'blastn', 'blastp', 'blastx', 'tblastn' or 'tblastx'
+            # # * _db_ (required): name of the local database
+            # # * _options_: blastall options \
+            # # (see http://www.genome.jp/dbget-bin/show_man?blast2)
+            # # * _blastall_: full path to blastall program (e.g. "/opt/bin/blastall"; DEFAULT: "blastall")
+            # # *Returns*:: Bio::Blast factory object
+            # def self.local(program, db, options = '', blastall = nil)
+            #   f = self.new(program, db, options, 'local')
+            #   if blastall then
+            #     f.blastall = blastall
+            #   end
+            #   f
+            # end
         blaster = Bio::Blast.local( program, "#{Rails.root}/index/blast/#{database}", blast_options)
-        puts blaster.inspect
+        # blaster = Bio::Blast.new( program, "#{Rails.root}/index/blast/#{database}", blast_options,"local")
+        # puts blaster.inspect
         report = blaster.query(@sequence.seq)
 
         hit_array = Hash.new
+
+
+        # puts "hit================"
+        report.each do |hit|
+          puts hit.evalue           # E-value
+          # puts hit.sw               # Smith-Waterman score (*)
+          puts hit.identity         # % identity
+          puts hit.overlap          # length of overlapping region
+          puts hit.query_id         # identifier of query sequence
+          puts hit.query_def        # definition(comment line) of query sequence
+          puts hit.query_len        # length of query sequence
+          puts hit.query_seq        # sequence of homologous region
+          puts hit.target_id        # identifier of hit sequence
+          puts hit.target_def       # definition(comment line) of hit sequence
+          puts hit.target_len       # length of hit sequence
+          puts hit.target_seq       # hit of homologous region of hit sequence
+          puts hit.query_start      # start position of homologous
+                                        # region in query sequence
+          puts hit.query_end        # end position of homologous region
+                                        # in query sequence
+          puts hit.target_start     # start position of homologous region
+                                        # in hit(target) sequence
+          puts hit.target_end       # end position of homologous region
+                                        # in hit(target) sequence
+          puts hit.lap_at           # array of above four numbers
+          puts "================"
+        end
 
         report.each do |hit|
           if hit.target_def =~ /(\d+)/
@@ -53,6 +98,13 @@ class QueryController < ApplicationController
             @hits[[$1.to_i, query_name]] = hit
           end
         end
+        # puts "@hits ================"
+        # @hits.each do |hit|
+        #   puts hit
+        #   puts "---"
+        # end
+
+
         # resultlist = sequence_class.find(hit_array.keys)
         # resultlist.sort! { |a, b| hit_array[b.id].bit_score <=> hit_array[a.id].bit_score }
 
@@ -69,7 +121,7 @@ class QueryController < ApplicationController
   private
 
   def set_search_defaults
-    params[:gap_cost] ||= '-1'
+    params[:gap_cost] ||= '2'
     params[:extend_cost] ||= '-1'
     params[:mismatch_penalty] ||= '-3'
     params[:match_reward] ||= '1'
