f5edc4511f36890aff9bf4fa3513621c8d8b16c6
jonaspatron <jonas.patron@mail.mcgill.ca>
2019-07-18 16:57:34 -0600

    changes

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -166,7 +166,7 @@ class QueryController < ApplicationController
     phy.close()
 
 
-    render json: { "tree": tree_data, "highlight": fasta_array.definition }
+    render json: { "tree": tree_data, "highlight": 'db_num' }
 
     # render #{Rails.root}/tmp/tmp_fasta/#{current_time}.phy
 
