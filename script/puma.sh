bcdf724c9ada43955e91c6f659c9b10bafc9481c
Danissss <danis.cao@hotmail.com>
2019-07-20 11:30:30 -0600

    print info puma.sh

--- script/puma.sh
+++ script/puma.sh
@@ -31,6 +31,7 @@ puma_is_running() {
 case "$1" in
   start)
     echo "Starting puma..."
+    pwd
     rm -f $PUMA_SOCKET
     bundle exec puma --daemon --bind unix://$PUMA_SOCKET --pidfile $PUMA_PID_FILE --config $PUMA_CONFIG_FILE
  
