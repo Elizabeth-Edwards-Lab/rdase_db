74edba4d634b41049a7bfc575220ea7588c178c0
Danissss <danis.cao@hotmail.com>
2019-10-30 12:40:46 -0600

    add compound and protein card

--- app/helpers/protein_helper.rb
+++ app/helpers/protein_helper.rb
@@ -13,8 +13,8 @@ module ProteinHelper
 
 	def style_chain(chain)
 		after_style = ""
-		length_per_line = 60
-		space_per_line = 10 # 10 sequence then space
+		length_per_line = 60 # 60 char then \n
+		space_per_line = 10 # 10 char then space
 		# {line number}<span></span>
 		after_style += "1 <span>"
 		(1..chain.length).step(1) do |n|
@@ -25,14 +25,13 @@ module ProteinHelper
 
 			if n%length_per_line == 0
 				# new line
-				# after_style += chain[n-1]
 				after_style +=  "</span><br>#{n+1} <span>"
 			end
 			after_style += chain[n-1]
 		end
 
 		after_style += "</span>"
-		
+
 		# convert string to html element
 		return after_style.html_safe
 	end
