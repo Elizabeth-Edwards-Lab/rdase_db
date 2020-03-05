052c2a53c2ec74a77af74df5616ac6d0459f03f8
Herbert Molenda <me@herbertm.ca>
2020-03-05 19:21:06 +0000

    content adjustments

--- app/admin/pubmed_references.rb
+++ app/admin/pubmed_references.rb
@@ -1,4 +1,11 @@
 ActiveAdmin.register PubmedReference, as: "Reference" do
   permit_params :pubmed_id, :citation, :doi, :url, :strain_id
-  
+  form do |f|
+    f.semantic_errors
+    f.inputs do
+        f.inputs :pubmed_id, :citation, :doi, :url
+        f.input :customized_protein_sequence
+    end
+    f.actions
+end
 end
